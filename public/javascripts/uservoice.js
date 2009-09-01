719a81efd775176845ce08fb3f795e8e0d27475a
Thomas Sommer <ts@192-168-1-7.tpgi.com.au>
2009-09-01 22:14:36 +1000

    Added a uservoice tab.

---
+++ public/javascripts/uservoice.js
@@ -0,0 +1,10 @@
+UserVoice.Tab.show({ 
+  key: 'vya',
+  host: 'vya.uservoice.com', 
+  forum: '27825',
+  alignment: 'right',
+  background_color:'#f00', 
+  text_color: 'white',
+  hover_color: '#06C',
+  lang: 'en'
+})
