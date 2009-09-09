82c3747a57924621304392eb7a92e7145f63ef73
Tom <tom@tom.local>
2009-09-10 09:50:26 +1000

    Added the name of the user who nominated the album to the upcoming list.

--- models/user.rb
+++ models/user.rb
@@ -5,5 +5,7 @@ class User
   property :name, String, :length => 100
   property :ip, String, :length => 20
   
+  def has_name?; name && name != "" end
+  
   def self.get_or_create_by(ip); first(:ip => ip) || create(:ip => ip) end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,26 @@ require File.join(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
   
+  describe "has name?" do
+    before do
+      @user = User.new
+    end
+    
+    it "should return false if the name is nil" do
+      @user.should_not have_name
+    end
+    
+    it "should return false if the name is empty" do
+      @user.name = ""
+      @user.should_not have_name
+    end
+    
+    it "should return true if we have a non empty string" do
+      @user.name = "me"
+      @user.should have_name
+    end
+  end
+  
   describe "get or create by" do
     before do
       User.stub!(:first).and_return @user = User.new
