6c431522cb5666be86537e65a5b771e2ef5ef785
Thomas Sommer <tom@-tombook-.(none)>
2010-07-09 16:41:42 +1000

    Change to nice time strings for log.

--- models/update.rb
+++ models/update.rb
@@ -7,6 +7,22 @@ class Update
 
   default_scope(:default).update :order => [:time.desc, :id.desc], :limit => 20
 
+  def time_ago
+    distance = ((DateTime.now - time) * 24 * 60).to_f.round
+
+    case distance
+      when 0               then "less than a minute ago"
+      when 1               then "1 minute ago"
+      when 2..44           then "#{distance} minutes ago"
+      when 45..89          then "1 hour ago"
+      when 90..1439        then "#{(distance.to_f / 60.0).round} hours ago"
+      when 1440..2529      then "1 day ago"
+      when 2530..43199     then "#{(distance.to_f / 1440.0).round} days ago"
+      when 43200..86399    then "1 month ago"
+      else                      "a long time ago"
+    end
+  end
+
   def self.log(action)
     create :action => action, :time => Time.now
   end
